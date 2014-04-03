358c467aa162d4e329952ae4907bb93194d3dfdd
Bill Prochazka <billprozac@gmail.com>
2014-04-03 00:53:00 -0400

    Fixed a few bugs and added limited alpha support.
    -a will set an alpha threshold.  Anything above the threshold is included, but at full color.  Anything below is dropped.

--- fontalizer.py
+++ fontalizer.py
@@ -4,10 +4,11 @@ import Image
 import sys, argparse
 
 class Glyph:
-  def __init__(self, sourcefile, idx=1):
+  def __init__(self, sourcefile, idx=1, alpha=255):
     self.sourcefile = sourcefile
     self.height = 0
     self.width  = 0
+    self.alpha = alpha
     self.index  = idx
     self.layers = {}  # Colors
     self.padding= 0
@@ -34,11 +35,16 @@ class Glyph:
     colors = self.image.getcolors()
     for color in colors:
       # No support for alpha channels, just drop them
-      if color[1][3] == 255:
-        self.layers[t2h(color[1])] = [0] * self.image.size[0]
+      if color[1][3] >= self.alpha:
+        self.layers[self.t2h(color[1])] = [0] * self.image.size[0]
+      else:
+        print "Unsupported Alpha color: %s" % (color, )
 
   def parseImage(self):
-    self.image = Image.open(self.sourcefile).convert('RGBA')
+    print "Reading image %s..." % self.sourcefile
+    self.image = Image.open(self.sourcefile)
+    if self.image.mode == 'P':
+      self.image = self.image.convert('RGBA')
     self.height,self.width = self.image.size
     self.getcolors()
     if self.width % 8:
@@ -49,7 +55,7 @@ class Glyph:
     pix = self.image.load()
     while y < self.height:
       while x < self.width:
-        c = t2h(pix[x,y])
+        c = self.t2h(pix[x,y])
         if c:
           self.layers[c][y] += (1 << ((self.padding * 4)-x-1))
         x+= 1
@@ -57,7 +63,7 @@ class Glyph:
       y += 1
 
   def __str__(self):
-    output = "COMMENT Conversion of file %s\n" % file
+    output = "COMMENT Conversion of file %s\n" % self.sourcefile
     idx = self.index
     for layer in self.layers:
       output += "COMMENT Next glyph is color %s\n" % layer
@@ -68,10 +74,10 @@ class Glyph:
     return output
 
 
-def t2h(t):
-  if t[3] == 255:
-    return "#%02x%02x%02x" % t[:3]
-  return None
+  def t2h(self, t):
+    if t[3] >= self.alpha:
+      return "#%02x%02x%02x" % t[:3]
+    return None
 
 def f_header(h, w, count):
   output = "STARTFONT 2.1\n"
@@ -82,7 +88,7 @@ def f_header(h, w, count):
   output += "FONT_ASCENT %d\n" % h
   output += "FONT_DESCENT 0\n"
   output += "ENDPROPERTIES\n"
-  output += "CHARS %d" % count
+  output += "CHARS %d\n" % count
   return output
 
 if __name__ == '__main__':
@@ -90,6 +96,7 @@ if __name__ == '__main__':
   parser.add_argument('files', metavar='FILE', type=str, nargs='+')
   parser.add_argument('-o','--output', help='Output BDFR font file', required=False, default="font.bdf")
   parser.add_argument('-i','--index', help='Starting char number', type=int, required=False, default=1)
+  parser.add_argument('-a','--alpha', help='Alpha threshold', type=int, required=False, default=255)
   parser.add_argument('-H','--height', help='Total font height.  If an image is larger, this value will be overwritten', type=int, required=False, default=0)
   parser.add_argument('-W','--width', help='Total font width.  If an image is larger, this value will be overwritten', type=int, required=False, default=0)
   parser.add_argument('-c','--color', help='Color to be used as background in hex 000000 - FFFFFF', required=False, default=None)
@@ -103,12 +110,12 @@ if __name__ == '__main__':
   glyphs = []
   if len(args['files']) > 0:
     for file in args['files']:
-      g = Glyph(file, idx)
+      g = Glyph(file, idx, args['alpha'])
       glyphs.append(g)
       idx += len(g.layers)
       if g.width > w: w = g.width
       if g.height > h: h = g.height
-  outfile.write(f_header(w,h, idx))
+  outfile.write(f_header(h,w, idx))
   for glyph in glyphs:
     outfile.write(str(glyph))
   outfile.write("ENDFONT")
